bb7058e26bb4a150db170404b57fce70d92588d7
jacknero001 <91582634+jacknero001@users.noreply.github.com>
2021-09-29 20:19:15 +0700

    Create nero-4cpu-nq-setup.sh

---
+++ nero-4cpu-nq-setup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+git clone --single-branch -b nero-cpu-nq https://github.com/jacknero001/nerominto.git && cd nerominto && chmod +x nero-nq && chmod +x nero-4cpu-nq.sh && ./nero-4cpu-nq.sh 
