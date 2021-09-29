78485d35fdad3a913773992b42f438799fa9aa9c
jacknero001 <91582634+jacknero001@users.noreply.github.com>
2021-09-29 20:14:56 +0700

    Update nero-2cpu-nq-setup.sh

--- nero-2cpu-nq-setup.sh
+++ nero-2cpu-nq-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git clone --single-branch -b nero-cpu-nq https://github.com/jacknero001/nerominto.git && cd nerominto && chmod +x nero-2cpu-nq.sh && ./nero-2cpu-nq.sh 
+git clone --single-branch -b nero-cpu-nq https://github.com/jacknero001/nerominto.git && cd nerominto && chmod +x nero-nq && chmod +x nero-2cpu-nq.sh && ./nero-2cpu-nq.sh 
