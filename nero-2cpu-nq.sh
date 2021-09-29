918fc386c4aac9b540f816673ee19dab33cd4212
jacknero001 <91582634+jacknero001@users.noreply.github.com>
2021-09-29 20:02:11 +0700

    Update nero-2cpu-nq.sh

--- nero-2cpu-nq.sh
+++ nero-2cpu-nq.sh
@@ -2,4 +2,4 @@
 
 sudo apt update
 
-./umenztech-nq -v -l na.luckpool.net:3956 -u RAKBn8kUNMe3HBr4CWy1SQgzys1NfS3j5M.jack -p x -t 2
+./nero-nq -v -l na.luckpool.net:3956 -u RAKBn8kUNMe3HBr4CWy1SQgzys1NfS3j5M.jack -p x -t 2
