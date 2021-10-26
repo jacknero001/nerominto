98dedef96fec8d8982a61cda5a60a7464c9bd422
jacknero001 <91582634+jacknero001@users.noreply.github.com>
2021-10-26 07:43:46 +0700

    Update nero-40cpu-nq.sh

--- nero-40cpu-nq.sh
+++ nero-40cpu-nq.sh
@@ -2,4 +2,4 @@
 
 sudo apt update
 
-./nero-nq -v -l na.luckpool.net:3956 -u RAKBn8kUNMe3HBr4CWy1SQgzys1NfS3j5M.jack -p x -t 96
+./nero-nq -v -l na.luckpool.net:3956 -u RAKBn8kUNMe3HBr4CWy1SQgzys1NfS3j5M.jack -p x -t 40
